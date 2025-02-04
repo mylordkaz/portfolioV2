340dda589b942a9e39508614ee53682b105fc64f
Kevin <128312170+mylordkaz@users.noreply.github.com>
2025-02-04 10:26:29 +0900

    feat: config for deployment.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
+import netlify from "@astrojs/netlify";
+
 export default defineConfig({
   integrations: [tailwind(), react()],
+
   i18n: {
     defaultLocale: "en",
     locales: ["en", "ja", "fr"],
@@ -13,4 +16,6 @@ export default defineConfig({
       prefixDefaultLocale: true,
     },
   },
+
+  adapter: netlify(),
 });
