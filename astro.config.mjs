ca6146eda45e282e85e548659d0a272dba29b687
Kevin <128312170+mylordkaz@users.noreply.github.com>
2025-08-30 23:48:05 +0900

    bundle opti

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
   site: "https://kevintim.com",
